0903626807ab90ca742059e4ae346f6f99622ac7
Gabrielly Tiberio <gabriellye627@gmail.com>
2022-09-16 16:52:24 -0300

    aula 16/09

---
+++ peixonauta.js
@@ -0,0 +1,19 @@
+alert("Código funcionando!")
+const nomedeusuario = prompt("Olá, qual o seu nome?")
+
+let num = Number(prompt("Olá," + nomedeusuario + ", Digite sua idade, por favor :)"))
+console.log(typeof num)
+
+//const sucessor = num + 1
+//const antecessor = num - 1
+
+//alert("O sucessor de " + num + ' é ' + sucessor)
+//alert('O antecessor de ' + num + ' é ' + antecessor)
+
+//ex. número 5
+console.log(num++) //5
+console.log(num) //6
+console.log(++num) //7
+console.log(log--) //7
+console.log(num) //6
+console.log(--num) //5
